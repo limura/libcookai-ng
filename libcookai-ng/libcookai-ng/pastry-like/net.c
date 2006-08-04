f1dce582e5c6f28a0d628b46fe3bdaaf637062d7
uirou.j <uirou.j@c449407d-291a-0410-96e5-036263b31150>
2006-08-04 18:47:03 +0000

    add prop Id

--- libcookai-ng/libcookai-ng/pastry-like/net.c
+++ libcookai-ng/libcookai-ng/pastry-like/net.c
@@ -22,7 +22,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * $Id: net.c,v 1.2 2004/12/16 16:09:36 takuji-i Exp $
+ * $Id$
  */
 
 #include "config.h"
